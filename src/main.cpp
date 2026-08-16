9ca1d324a190be6af55c9bc7052ce3ce449e1faa
Nowelix <maksumprocuk@gmail.com>
2026-08-16 18:14:54 +0300

    fix popup showing on wrong scene

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,12 @@
 #include <Geode/Geode.hpp>
-#include <Geode/modify/MenuLayer.hpp>
 
 using namespace geode::prelude;
 
-class $modify(HelloMenuLayer, MenuLayer) {
-    bool init() {
-        if (!MenuLayer::init()) {
-            return false;
-        }
-
-        // Показуємо плашку одразу при вході в головне меню
-        FLAlertLayer::create(
-            "Hello",          // заголовок
-            "Hello World!",   // текст
-            "OK"               // текст кнопки
-        )->show();
-
-        return true;
-    }
-};
+$on_game(Loaded) {
+    auto alert = FLAlertLayer::create(
+        "Hello",
+        "Hello World!",
+        "OK"
+    );
+    alert->show();
+}
